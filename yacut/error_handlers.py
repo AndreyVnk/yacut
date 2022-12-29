2590acd49fced8756a5235c0191a8df2da0f2d38
Andrei Bychkov <andrey.bychkov.fx@gmail.com>
2022-12-29 12:40:34 +0400

    Fix error_handlers

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -1,16 +1,17 @@
+from http import HTTPStatus
+
 from flask import jsonify, render_template
 
 from . import app
 
 
 class InvalidAPIUsage(Exception):
-    status_code = 400
 
     def __init__(self, message, status_code=None):
         super().__init__()
         self.message = message
         if status_code is not None:
-            self.status_code = status_code
+            self.status_code = HTTPStatus.BAD_REQUEST
 
     def to_dict(self):
         return dict(message=self.message)
@@ -23,4 +24,4 @@ def invalid_api_usage(error):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('core/404.html'), 404
+    return render_template('core/404.html'), HTTPStatus.NOT_FOUND
